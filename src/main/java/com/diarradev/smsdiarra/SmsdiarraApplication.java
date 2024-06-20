2861aa7e3ee6ba77164e66c41d2fa14cb6f6de49
Moise DIARRA <moisepr@gmail.com>
2024-06-20 13:00:55 +0000

    Create Stock Management System Diarra

---
+++ src/main/java/com/diarradev/smsdiarra/SmsdiarraApplication.java
@@ -0,0 +1,13 @@
+package com.diarradev.smsdiarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsdiarraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsdiarraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diarradev/smsdiarra/SmsdiarraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarradev.smsdiarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsdiarraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
